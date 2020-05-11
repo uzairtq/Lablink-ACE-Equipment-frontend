6c2622d3c2d08ed8d2c5121969a3a9e54f8f6791
uzairtq <uzairtq@gmail.com>
2020-05-11 11:38:56 -0400

    Store for data created

--- src/components/samples/Samples.js
+++ src/components/samples/Samples.js
@@ -2,6 +2,7 @@ import React from "react";
 import sampleApi from "../../resources/sampleApi";
 import SampleTable from "./SampleTable";
 import SampleForm from "./SampleForm";
+import { sample_list_data } from "../store.js";
 
 class Samples extends React.Component {
   state = {
@@ -23,103 +24,7 @@ class Samples extends React.Component {
   };
 
   fetchSampleList = () => {
-    //    sampleApi
-    //      .getHomepage()
-    //      .then(res => {
-    this.setState({
-      status: "success",
-      data: [
-        {
-          serialNumber: null,
-          description: "sample",
-          submittedDate: "2020-05-07T14:01:10.292000",
-          siteCode: "IK",
-          _links: {
-            self: "/api/samples/IK20_00002"
-          },
-          testRequests: [],
-          productNumber: null,
-          owner: "tom.jones@example.com",
-          id: "IK20_00002",
-          image: null,
-          softwareVersion: null,
-          assignee: "tom.jones@example.com",
-          alias: "sample 1"
-        },
-        {
-          serialNumber: null,
-          description: "sample",
-          submittedDate: "2020-05-07T14:01:24.665000",
-          siteCode: "IK",
-          _links: {
-            self: "/api/samples/IK20_00003"
-          },
-          testRequests: [],
-          productNumber: null,
-          owner: "tom.jones@example.com",
-          id: "IK20_00003",
-          image: null,
-          softwareVersion: null,
-          assignee: "tom.jones@example.com",
-          alias: "sample 2"
-        },
-        {
-          serialNumber: null,
-          description: "sample",
-          submittedDate: "2020-05-07T14:01:28.376000",
-          siteCode: "IK",
-          _links: {
-            self: "/api/samples/IK20_00004"
-          },
-          testRequests: [],
-          productNumber: null,
-          owner: "tom.jones@example.com",
-          id: "IK20_00004",
-          image: null,
-          softwareVersion: null,
-          assignee: "tom.jones@example.com",
-          alias: "sample 3"
-        },
-        {
-          serialNumber: null,
-          description: "sample",
-          submittedDate: "2020-05-07T14:01:32.087000",
-          siteCode: "IK",
-          _links: {
-            self: "/api/samples/IK20_00005"
-          },
-          testRequests: [],
-          productNumber: null,
-          owner: "tom.jones@example.com",
-          id: "IK20_00005",
-          image: null,
-          softwareVersion: null,
-          assignee: "tom.jones@example.com",
-          alias: "sample 4"
-        },
-        {
-          serialNumber: null,
-          description: "sample",
-          submittedDate: "2020-05-07T14:01:37.607000",
-          siteCode: "IK",
-          _links: {
-            self: "/api/samples/IK20_00006"
-          },
-          testRequests: [],
-          productNumber: null,
-          owner: "tom.jones@example.com",
-          id: "IK20_00006",
-          image: null,
-          softwareVersion: null,
-          assignee: "tom.jones@example.com",
-          alias: "sample 5"
-        }
-      ]
-    });
-    //    console.log(this.sampleData);
-    //        console.log(res.data.data);
-    //      })
-    //      .catch(err => console.log(err));
+    this.setState({ sampleData: sample_list_data.data });
   };
 
   handleSampleInputClose = () =>
@@ -136,14 +41,10 @@ class Samples extends React.Component {
         })
         .catch(err => console.log(err));
     } else {
-      ////      sampleApi
-      //       .createNew(sampleData)
-      //        .then(res => {
       let samplesList = this.state.sampleData;
       samplesList.push(sampleData);
       this.setState({ sampleData: samplesList });
-      //        })
-      //       .catch(err => console.log(err));
+      console.log(sampleData);
     }
   };
 
---
+++ src/components/store.js
@@ -0,0 +1,90 @@
+export const sample_list_data = {
+  status: "success",
+  data: [
+    {
+      serialNumber: null,
+      description: "sample",
+      submittedDate: "2020-05-07T14:01:10.292000",
+      siteCode: "IK",
+      _links: {
+        self: "/api/samples/IK20_00002"
+      },
+      testRequests: [],
+      productNumber: null,
+      owner: "tom.jones@example.com",
+      id: "IK20_00002",
+      image: null,
+      softwareVersion: null,
+      assignee: "tom.jones@example.com",
+      alias: "sample 1"
+    },
+    {
+      serialNumber: null,
+      description: "sample",
+      submittedDate: "2020-05-07T14:01:24.665000",
+      siteCode: "IK",
+      _links: {
+        self: "/api/samples/IK20_00003"
+      },
+      testRequests: [],
+      productNumber: null,
+      owner: "tom.jones@example.com",
+      id: "IK20_00003",
+      image: null,
+      softwareVersion: null,
+      assignee: "tom.jones@example.com",
+      alias: "sample 2"
+    },
+    {
+      serialNumber: null,
+      description: "sample",
+      submittedDate: "2020-05-07T14:01:28.376000",
+      siteCode: "IK",
+      _links: {
+        self: "/api/samples/IK20_00004"
+      },
+      testRequests: [],
+      productNumber: null,
+      owner: "tom.jones@example.com",
+      id: "IK20_00004",
+      image: null,
+      softwareVersion: null,
+      assignee: "tom.jones@example.com",
+      alias: "sample 3"
+    },
+    {
+      serialNumber: null,
+      description: "sample",
+      submittedDate: "2020-05-07T14:01:32.087000",
+      siteCode: "IK",
+      _links: {
+        self: "/api/samples/IK20_00005"
+      },
+      testRequests: [],
+      productNumber: null,
+      owner: "tom.jones@example.com",
+      id: "IK20_00005",
+      image: null,
+      softwareVersion: null,
+      assignee: "tom.jones@example.com",
+      alias: "sample 4"
+    },
+    {
+      serialNumber: null,
+      description: "sample",
+      submittedDate: "2020-05-07T14:01:37.607000",
+      siteCode: "IK",
+      _links: {
+        self: "/api/samples/IK20_00006"
+      },
+      testRequests: [],
+      productNumber: null,
+      owner: "tom.jones@example.com",
+      id: "IK20_00006",
+      image: null,
+      softwareVersion: null,
+      assignee: "tom.jones@example.com",
+      alias: "sample 5"
+    }
+  ]
+};
